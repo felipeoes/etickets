b20a0c45baff5cd12ad6f14989b52340163a7735
Felipe Oliveira <felipeoes@usp.br>
2023-05-29 21:47:59 -0300

    feat: add user interests endpoint

--- proj/urls.py
+++ proj/urls.py
@@ -7,8 +7,11 @@ from create_fake_data import create_fake_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # USERS
     path('users/list/', UsersList.as_view()),
     path('users/<str:email>/', UserDetail.as_view()),
+    path('users-interests/', UserInterest.as_view()),
 
     # TICKETS
     path('tickets/list/', TicketsList.as_view()),
